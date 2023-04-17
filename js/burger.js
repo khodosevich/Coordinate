c35e8f3235afed708eefbc2c40e019208594dc12
Matvey <hodos.952795@gmail.com>
2023-04-17 23:15:24 +0300

    init project

---
+++ js/burger.js
@@ -0,0 +1,4 @@
+document.getElementById("btn-burger").addEventListener("click", function (){
+    console.log("hello")
+   document.getElementById("navbar").classList.toggle("open");
+});
---
+++ js/modal.js
@@ -0,0 +1,42 @@
+const openModal = document.querySelectorAll(".contact-us");
+const closeModal = document.getElementById("close");
+
+
+for (let i = 0; i < openModal.length; i++) {
+    openModal[i].addEventListener("click", function (){
+        document.getElementById("modal").classList.add("open");
+    });
+}
+
+closeModal.addEventListener("click", function (){
+    document.getElementById("modal").classList.remove("open");
+});
+
+
+
+//-----login-modal------------
+
+const loginModal = document.getElementById("login");
+const loginClose = document.getElementById("loginClose");
+
+loginModal.addEventListener("click", function (){
+   document.querySelector(".modalLogin") .classList.add('open');
+});
+
+loginClose.addEventListener("click", function (){
+    document.querySelector(".modalLogin") .classList.remove('open');
+});
+
+//---------learn-modal----------------
+
+
+const learnModal = document.getElementById("learnBtn");
+const learnClose = document.getElementById("learnClose");
+
+learnModal.addEventListener("click", function (){
+    document.querySelector(".learnModal") .classList.add('open');
+});
+
+learnClose.addEventListener("click", function (){
+    document.querySelector(".learnModal") .classList.remove('open');
+});
